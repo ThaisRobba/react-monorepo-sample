8a517023a0bc14bcd3fe15afc7ac046045e6ffc8
ThaisRobba <thais.robba@gmail.com>
2019-04-29 23:16:45 -0300

    Fixed wrong script type

--- simple-page/public/main.js
+++ simple-page/public/main.js
@@ -1,17 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-// import App from '../src'
+import App from '../src/index.js'
 
-const App = () => {
-  return (
-    <div>
-      <h1>hello</h1>
-      {/* <p>{process.env.SOME_URL}</p>
-        <img src={Cat} />
-        <SimpleComponent /> */}
-    </div>
-  )
-}
-console.log('hello')
 ReactDOM.render(<App />, document.getElementById('root'))
